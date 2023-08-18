541b5680796248f8df6f363b31aae0abe06b9ce9
Dominic <lovric.domagoj1@gmail.com>
2023-08-18 23:51:58 +0100

    Featured added to the app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navb from "./pages/Navb"; // Import the Navb component
 import Hero from "./pages/Hero";
+import Featured from "./pages/Featured";
 import './App.css';
 
 
@@ -11,6 +12,7 @@ function App() {
       <div className="App">
         <Navb />
         <Hero />
+        <Featured />
       </div>
     </Router>
   );
