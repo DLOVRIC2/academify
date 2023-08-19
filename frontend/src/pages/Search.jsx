0cd2082ddae09c781d771e0fe4023e74671e3088
Dominic <lovric.domagoj1@gmail.com>
2023-08-20 00:42:29 +0100

    Updated search

--- frontend/src/pages/Search.jsx
+++ frontend/src/pages/Search.jsx
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from "react";
+import { useNavigate } from 'react-router-dom';
+import './style/Search.css';
 
 const Search = () => {
+  const navigate = useNavigate(); 
   const [searchQuery, setSearchQuery] = useState("");
   const [articles, setArticles] = useState([]);
   const [selectedArticle, setSelectedArticle] = useState(null);
@@ -17,28 +20,28 @@ const Search = () => {
     setSearchQuery(tag);
   };
 
-  const handleSearch = () => {
+  const handleSearch = (e) => {
+    
+    e.preventDefault(); // Prevent default form submission
     let url;
     if (predefinedTags.includes(searchQuery)) {
       url = `http://localhost:8000/search/tag/${searchQuery}`;
     } else {
       url = `http://localhost:8000/search/title/${searchQuery}`;
     }
-  
-fetch(url)
-  .then((response) => response.json())
-  .then((data) => {
-    if (typeof data === "string") {
-      data = JSON.parse(data);
-    }
-    console.log("Received data:", data); // log the data
-    setArticles(data); // Set the state directly with the parsed data
-  })
-  .catch((error) => console.error("Error fetching data:", error));
 
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        if (typeof data === "string") {
+          data = JSON.parse(data);
+        }
+        console.log("Received data:", data);
+        setArticles(data);
+      })
+      .catch((error) => console.error("Error fetching data:", error));
   };
-  
-  
+
   useEffect(() => {
     console.log("Articles state changed:", articles);
     console.log("Type of articles state:", typeof articles);
@@ -46,26 +49,33 @@ fetch(url)
       console.error("Articles state is not an array!");
     }
   }, [articles]);
-  
-  
-  
 
   const handleRowClick = (article) => {
     setSelectedArticle(article);
   };
 
+  const handleGo = () => {
+    if (selectedArticle) {
+        navigate(`/article/${selectedArticle.title}`);
+    }
+  };
+
   return (
-    <div>
-      <h1>Search the topic you are interested in</h1>
-      <input
-        type="text"
-        value={searchQuery}
-        onChange={(e) => setSearchQuery(e.target.value)}
-      />
-      <button onClick={handleSearch}>Search</button>
+    <div className="table-container">
+      <h1 className="search-heading">Search the topic you are interested in</h1>
+      <form onSubmit={handleSearch}> {/* Wrap the input and buttons in a form */}
+        <input
+          type="text"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+          className="search-input"
+        />
+        <button type="submit" className="search-button">Search</button>
+        <button type="button" onClick={handleGo} className="search-button">Go</button>
+      </form>
       <div>
         {predefinedTags.map((tag) => (
-          <button key={tag} onClick={() => handleTagClick(tag)}>
+          <button key={tag} onClick={() => handleTagClick(tag)} className="tag-button">
             {tag}
           </button>
         ))}
@@ -80,10 +90,14 @@ fetch(url)
         </thead>
         <tbody>
           {articles.map((article) => (
-            <tr key={article.title} onClick={() => handleRowClick(article)}>
-              <td>{article.title}</td>
-              <td>{article.authors.join(", ")}</td>
-              <td>{article.published}</td>
+            <tr
+            key={article.title}
+            onClick={() => handleRowClick(article)}
+            className={selectedArticle === article ? "selected-row" : ""}
+            >              
+                <td>{article.title}</td>
+                <td>{article.authors.join(", ")}</td>
+                <td>{article.published}</td>
             </tr>
           ))}
         </tbody>
