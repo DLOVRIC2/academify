fcdbc9938ebd04f26d11fa60337009693424d5be
Dominic <lovric.domagoj1@gmail.com>
2023-08-19 22:25:40 +0100

    Added search jsx

---
+++ frontend/src/pages/Search.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Search = () => {
+  return (
+    <div>
+      <h1>Search Page</h1>
+      {/* Add your search functionality here */}
+    </div>
+  );
+};
+
+export default Search;
