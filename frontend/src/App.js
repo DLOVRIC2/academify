aced17c31dc36d8a8fe389f5a689c4e9e952cc10
Dominic <lovric.domagoj1@gmail.com>
2023-08-19 22:25:34 +0100

    Rendering just body jsx instead of components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Navb from "./pages/Navb"; // Import the Navb component
+import Navb from "./pages/Navb";
 import Hero from "./pages/Hero";
 import Featured from "./pages/Featured";
+import Search from "./pages/Search";
+import Body from "./pages/Body"; // Import the Body component
 import './App.css';
 
 
@@ -11,8 +13,10 @@ function App() {
     <Router>
       <div className="App">
         <Navb />
-        <Hero />
-        <Featured />
+        <Routes>
+          <Route path="/search" element={<Search />} />
+          <Route path="*" element={<Body />} />
+        </Routes>
       </div>
     </Router>
   );
