769c70af6b07c65238830b235de811a373ee6760
Dominic <lovric.domagoj1@gmail.com>
2023-08-20 00:56:22 +0100

    Applying html for styling on articlepage

--- frontend/src/pages/ArticlePage.jsx
+++ frontend/src/pages/ArticlePage.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useLocation } from 'react-router-dom';
+import './style/ArticlePage.css';
 
 const ArticlePage = () => {
   const location = useLocation();
@@ -10,10 +11,11 @@ const ArticlePage = () => {
   }
 
   return (
-    <div>
-      <h1>Title: {article.title}</h1>
-      <h2>Authors: {article.authors.join(', ')}</h2>
-      <p>Abstract: {article.abstract}</p>
+    <div className="article-container">
+      <h1 className="article-title">Title: {article.title}</h1>
+      <h2 className="article-authors">Authors: {article.authors.join(', ')}</h2>
+      <p className="article-abstract">Abstract: {article.abstract}</p>
+      <a href={article.pdf_url} target="_blank" rel="noopener noreferrer" className="article-pdf-link">Download PDF</a>
     </div>
   );
 };
