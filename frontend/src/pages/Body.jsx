50bb993e9ecd4a43dbfee8de2aa916e611f5afc4
Dominic <lovric.domagoj1@gmail.com>
2023-08-19 22:25:22 +0100

    Moved hero and featured into a body jsx

---
+++ frontend/src/pages/Body.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Hero from "./Hero";
+import Featured from "./Featured";
+
+const Body = () => {
+  return (
+    <div>
+      <Hero />
+      <Featured />
+    </div>
+  );
+};
+
+export default Body;
