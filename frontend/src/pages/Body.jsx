f0f19fb0035a551b34cd810b35aa2f45d4846299
Dominic <lovric.domagoj1@gmail.com>
2023-08-20 22:55:39 +0100

    Added about section to the body

--- frontend/src/pages/Body.jsx
+++ frontend/src/pages/Body.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import Hero from "./Hero";
 import Featured from "./Featured";
+import AboutSection from "./AboutSection"; // Import the AboutSection component
+
 
 const Body = () => {
   return (
     <div>
       <Hero />
       <Featured />
+      <AboutSection /> {/* Include the AboutSection component */}=
     </div>
   );
 };
