2237cbafecb03caee4a8f3d336851ac881db369a
Dominic <lovric.domagoj1@gmail.com>
2023-08-20 13:15:52 +0100

    Working version without streaming

--- frontend/src/pages/ArticlePage.jsx
+++ frontend/src/pages/ArticlePage.jsx
@@ -1,17 +1,30 @@
 import React, { useState } from "react";
 import { useLocation } from 'react-router-dom';
 import './style/ArticlePage.css';
+import ReactLoading from 'react-loading';
+import robot from "./images/robot.png";
+import human from "./images/human.png";
+
 
 const ArticlePage = () => {
   const location = useLocation();
   const article = location.state?.article;
   const [showChat, setShowChat] = useState(false);
-  const [chatHistory, setChatHistory] = useState([]);
+  // Initialize the chat history with the bot's greeting
+  const [chatHistory, setChatHistory] = useState([
+    {
+      user: 'Bot',
+      text: "Hey! Ask me any question about this paper. I'll do my best to help.",
+    },
+  ]);
   const [messageInput, setMessageInput] = useState('');
   const [sessionId, setSessionId] = useState(null);
+  const [loading, setLoading] = useState(false);
+
 
 
   const handleQAButtonClick = async () => {
+    setLoading(true);
     try {
       const response = await fetch('http://localhost:8000/start_conversation', { 
         method: 'POST',
@@ -21,6 +34,7 @@ const ArticlePage = () => {
       const data = await response.json();
       setSessionId(data.session_id);
       setShowChat(true);
+      setLoading(false);
     } catch (error) {
       console.error('Error starting conversation:', error);
     }
@@ -29,22 +43,24 @@ const ArticlePage = () => {
   const handleSendMessage = async () => {
     if (messageInput.trim() === '' || !sessionId) return; // Prevent sending empty messages
   
+    // Add the user's message to the chat history immediately
+    setChatHistory([...chatHistory, { user: 'User', text: messageInput }]);
+    setMessageInput(''); // Clear the input field
+  
     try {
       const response = await fetch(`http://localhost:8000/chat/${sessionId}?message=${encodeURIComponent(messageInput)}`, {
         method: 'GET'
       });
-      
-      const responseText = await response.text(); // Get the response as text
-      console.log('Response text:', responseText); // Log the response text
   
-      // Uncomment the following lines once you've figured out the issue
-      const botResponse = JSON.parse(responseText); // Parse the response text as JSON
+      const responseText = await response.text();
+      const botResponse = JSON.parse(responseText);
       setChatHistory([...chatHistory, { user: 'User', text: messageInput }, { user: 'Bot', text: botResponse }]);
-      setMessageInput(''); // Clear the input field
     } catch (error) {
       console.error('Error sending message:', error);
     }
   };
+  
+  
   
   
   const handleInputChange = (e) => {
@@ -68,25 +84,38 @@ const ArticlePage = () => {
           <button className="article-button tweet-button">Tweet</button>
         </div>
       </div>
-      {showChat && (
-        <div className="chat-window">
-          <div className="chat-container">
-            <div className="chat-history">
-              {chatHistory.map((message, index) => (
-                <div key={index} className={`chat-message ${message.user.toLowerCase()}`}>
-                  <span className="chat-user">{message.user}:</span> {message.text}
-                </div>
-              ))}
-            </div>
-            <input
-              type="text"
-              value={messageInput}
-              onChange={handleInputChange}
-              className="chat-input"
-              placeholder="Type your question..."
-              onKeyDown={(e) => e.key === 'Enter' && handleSendMessage()}
-            />
+        {loading && (
+          <div className="loading-container">
+            <ReactLoading className="loader" type={"balls"} color={"#333"} />
+            <div className="loading-message">Processing your article</div>
           </div>
+        )}
+
+      {showChat && (
+        <div className="chat-container">
+          <div className="chat-history">
+            {chatHistory.length === 0 && (
+              <div className="chat-message bot">
+                <img src={robot} alt="Bot" className="chat-icon" /> {/* Bot icon */}
+                <span className="chat-user">Bot:</span> Hey! Ask me any question about this paper. I'll do my best to help.
+              </div>
+            )}
+            {chatHistory.map((message, index) => (
+              <div key={index} className={`chat-message ${message.user.toLowerCase()}`}>
+                <img src={message.user === 'Bot' ? robot : human} alt={message.user} className="chat-icon" /> {/* Conditional rendering of User/Bot icon */}
+                <span className="chat-user">{message.user}:</span> {message.text}
+              </div>
+            ))}
+
+          </div>
+          <input
+            type="text"
+            value={messageInput}
+            onChange={handleInputChange}
+            className="chat-input"
+            placeholder="Type your question..."
+            onKeyDown={(e) => e.key === 'Enter' && handleSendMessage()}
+          />
         </div>
       )}
     </div>
