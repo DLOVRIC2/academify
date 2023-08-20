fefaa8905939c71b249141362605913643e086b8
Dominic <lovric.domagoj1@gmail.com>
2023-08-20 20:18:42 +0100

    CORS update

--- backend/main.py
+++ backend/main.py
@@ -20,6 +20,8 @@ origins = [
     "http://localhost:3000",
     "http://localhost:8000",
     "http://3.94.209.49:3000",
+    "http://backend:8000",
+    "http://academify_backend_1:8000",
 
 ]
 app.add_middleware(
